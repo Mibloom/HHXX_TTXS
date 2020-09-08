5751c8f381dc4205803fd61ba9b6c6038499b4c4
mibloom <mibloom@foxmail.com>
2020-09-08 17:02:03 +0800

    添加mibloom项目

---
+++ mibloom/src/test/java/com/mibloom/java/straw/StrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibloom.java.straw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
